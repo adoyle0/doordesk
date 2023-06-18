a73f6555597b59c998d57c0bc5d1750cea58a0a2
Adam <24621027+adoyle0@users.noreply.github.com>
2023-06-18 18:11:00 -0400

    docker

--- dennis/main.py
+++ dennis/main.py
@@ -3,4 +3,4 @@ import multiprocessing
 
 if __name__ == "__main__":
     multiprocessing.freeze_support()
-    uvicorn.run('src.dennis:api', host="127.0.0.1", port=9696, reload=True)
+    uvicorn.run('src.dennis:api', host='0.0.0.0', port=9696, reload=False)
