c66e4b86c42678bc56632c3fc0fd51875a9e93f6
Adam <24621027+adoyle0@users.noreply.github.com>
2023-06-26 20:34:43 -0400

    accomodate new backend

--- frontend/src/components/Slingshot.tsx
+++ frontend/src/components/Slingshot.tsx
@@ -12,15 +12,15 @@ export type Ammo = {
 export default function Slingshot(props: { ammo: Ammo }) {
 
     return (
-        <div class="bg-zinc-700 mx-auto p-7 my-5 w-11/12 max-w-screen-xl rounded-md shadow-lg bg-opacity-10">
+        <article class="bg-zinc-700 mx-auto p-7 my-5 w-11/12 max-w-screen-xl rounded-md shadow-lg bg-opacity-10">
             <h1 class="max-6-xs text-3xl text-orange-600 font-light capitalize">
                 {props.ammo.title}
             </h1>
             <hr class="opacity-50" />
-            <p class="opacity-50 text-xs pt-1.5 pb-3.5">
+            <span class="opacity-50 text-xs pt-0 m-t pb-3.5">
                 {props.ammo.date}
-            </p>
+            </span>
             <div innerHTML={props.ammo.content} />
-        </div>
+        </article>
     ) as JSXElement;
 };
