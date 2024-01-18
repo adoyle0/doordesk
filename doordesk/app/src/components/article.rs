30f3ee06967b2013ad39b93113b6819e6d486982
Adam <24621027+adoyle0@users.noreply.github.com>
2024-01-18 17:36:59 -0500

    format

--- doordesk/app/src/components/article.rs
+++ doordesk/app/src/components/article.rs
@@ -3,17 +3,12 @@ use leptos::*;
 
 #[component]
 pub fn Article(data: ArticleData) -> impl IntoView {
-
     view! {
-        <Transition>
-            <article class="p-7 my-5 mx-auto w-11/12 max-w-screen-xl bg-opacity-10 rounded-md bg-zinc-700 shadow-1g">
-                <h1 class="text-3xl font-light text-orange-600 capitalize max-6-xs">
-                    {&data.title}
-                </h1>
-                <hr class="opacity-50"/>
-                <span class="pt-0 pb-3.5 text-xs opacity-50 m-t">{&data.date}</span>
-                <div inner_html={&data.content}></div>
-            </article>
-        </Transition>
+        <article class="p-7 my-5 mx-auto w-11/12 max-w-screen-xl bg-opacity-10 rounded-md bg-zinc-700 shadow-1g">
+            <h1 class="text-3xl font-light text-orange-600 capitalize max-6-xs">{&data.title}</h1>
+            <hr class="opacity-50"/>
+            <span class="pt-0 pb-3.5 text-xs opacity-50 m-t">{&data.date}</span>
+            <div inner_html=&data.content></div>
+        </article>
     }
 }
