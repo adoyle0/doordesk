7a98b2c3c4ffd05834d831dcf3782324d9f44113
Adam <24621027+adoyle0@users.noreply.github.com>
2024-01-18 17:38:43 -0500

    give slingshot the path (change later (maybe (hopefully)))

--- doordesk/app/src/routes/home.rs
+++ doordesk/app/src/routes/home.rs
@@ -1,22 +1,30 @@
 use crate::components::article::*;
 use crate::components::slingshot::*;
+use crate::error_template::*;
 use leptos::*;
 
 #[component]
 pub fn Home() -> impl IntoView {
-    let data_resource = create_local_resource(|| (), |_| async move { slingshot().await });
+    let data_resource = create_local_resource(
+        || (),
+        |_| async move { slingshot("./public/static".to_string()).await },
+    );
 
     let articles_view = move || {
         data_resource.and_then(|data| {
             data.iter()
-            .map(|article| view! { <Article data=article.to_owned()/> })
-            .collect_view()
+                .map(|article| view! { <Article data=article.to_owned()/> })
+                .collect_view()
         })
     };
 
     view! {
         <Suspense fallback=move || {
             view! { <p>"Loading..."</p> }
-        }>{articles_view}</Suspense>
+        }>
+            <ErrorBoundary fallback=|errors| {
+                view! { <ErrorTemplate errors=errors/> }
+            }>{articles_view}</ErrorBoundary>
+        </Suspense>
     }
 }
