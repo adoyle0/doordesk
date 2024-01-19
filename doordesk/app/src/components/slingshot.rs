19a4c8e175c5380754bd9a2cba61a266c692b641
Adam <24621027+adoyle0@users.noreply.github.com>
2024-01-19 17:49:12 -0500

    use femark

--- doordesk/app/src/components/slingshot.rs
+++ doordesk/app/src/components/slingshot.rs
@@ -1,5 +1,5 @@
-use leptos::*;
 use crate::components::article::ArticleData;
+use leptos::*;
 
 #[server]
 pub async fn slingshot(path: String) -> Result<Vec<ArticleData>, ServerFnError> {
@@ -13,13 +13,17 @@ pub async fn slingshot(path: String) -> Result<Vec<ArticleData>, ServerFnError> 
             if let Some(filetype) = filepath.extension() {
                 if filetype == "md" {
                     let file = std::fs::read_to_string(filepath)?;
-                    let md1: String = markdown::to_html(&file);
+                    let html_from_md = femark::process_markdown_to_html(&file.to_string())
+                        .expect("Problem processing markdown");
+                    let content = html_from_md.content;
+                    let _toc = html_from_md.toc;
+                    let _frontmatter = html_from_md.frontmatter;
 
                     articles.push(ArticleData {
                         content_type: String::from("Blog"),
                         title: String::from("Test article"),
                         date: String::from("12/21/2022"),
-                        content: md1,
+                        content,
                     })
                 }
             }
