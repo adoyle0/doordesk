ee4b2c0cedc38c9b178a3b01b13020d723ad2964
Adam <24621027+adoyle0@users.noreply.github.com>
2024-01-19 22:36:03 -0500

    shadows for articles

--- doordesk/app/src/components/article.rs
+++ doordesk/app/src/components/article.rs
@@ -12,7 +12,8 @@ pub struct ArticleData {
 #[component]
 pub fn Article(data: ArticleData) -> impl IntoView {
     view! {
-        <article class="p-7 my-5 mx-auto w-11/12 max-w-screen-xl bg-opacity-10 rounded-md bg-zinc-700 shadow-1g">
+        // Shadow styling is in main .css file
+        <article class="p-7 my-5 mx-auto w-11/12 max-w-screen-xl bg-opacity-10 rounded-md bg-zinc-700">
             <h1 class="text-3xl font-light text-orange-600 capitalize max-6-xs">{&data.title}</h1>
             <hr class="opacity-50"/>
             <span class="pt-0 pb-3.5 text-xs opacity-50 m-t">{&data.date}</span>
